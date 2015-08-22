5ac47413e9a9682c438d839898be86dba31ac5bc
Jordan Lord <jordan@macmini>
2015-08-22 10:42:53 +0100

    + sprite_sheet.png + Tiles now show image

--- src/init.js
+++ src/init.js
@@ -5,17 +5,19 @@ var gameHeight = 240;
 
 game.set.canvas("gameCanvas", gameWidth, gameHeight);
 
+game.load.spriteSheet("mainSheet", "res/sprite_sheet.png", 16, 16);
+
 var testRect = new game.Rect(0, 0, 32, 32, "#000000");
 
 
 for (var x = -10; x < 10; x++) {
 	for (var y = -10; y < 10; y++) {
-		var tile = new game.Rect(x*32, y*32, 32, 32, "#000000");
+		var tile = new game.Sprite();
+		tile.x = x*32;
+		tile.y = y*32;
 
 		if (Math.random() > 0.5) {
-			tile.colour = "#FF0000";
-		} else {
-			tile.colour = "#00FF00";
+			tile.image = "mainSheet_0";
 		}
 
 	}
