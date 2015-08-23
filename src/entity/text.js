63484ab8700d199e65c17656f9916ddd6d403441
Jordan Lord <jordan@macmini>
2015-08-23 21:35:41 +0100

    = Text constructor complete

--- src/entity/text.js
+++ src/entity/text.js
@@ -8,21 +8,58 @@ var Text = function(text, x, y) {
 	var letter = [];
 	var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890,.abcdefghijklmnopqrstuvwxyz"
 
-	for (var i = 0; i < text.length; i++) {
-		var index = alphabet.indexOf(text.substring(i, i+1));
 
-		if (index != -1) {
-			letter[i] = game.Sprite();
-			letter[i].image = "font_" + index;
-			letter[i].x = x + i * 8;
-			letter[i].still = true;
+	function drawString(text) {
+		var length;
+
+		if (text.length > letter.length) {
+			length = text.length;
+		} else {
+			length = letter.length
 		}
-	}
+
+		for (var i = 0; i < length; i++) {
+			var index;
+
+			if (text.substring(i, i+1) != "") {
+				index = alphabet.indexOf(text.substring(i, i+1));
+			} else {
+				index = -1;
+			}
+
+			if (letter[i] !== undefined) {
+				letter[i].destroy();
+			}
+
+			if (index != -1) {
+				letter[i] = game.Sprite();
+				letter[i].image = "font_" + index;
+				letter[i].x = x + i * 8;
+				letter[i].still = true;
+			}
+		}
+	};
+
+	drawString(text);
 
 	mod.updateXY = function(x, y) {
 		for (var i = 0; i < text.length; i++) {
-			letter[i].x = x + i * 8;
-			letter[i].y = y;
+			if (letter[i] !== undefined) {
+				letter[i].x = x + i * 8;
+				letter[i].y = y;
+			}
+		}
+	};
+
+	mod.updateText = function(text) {
+		drawString(text);
+	};
+
+	mod.destroy = function() {
+		for (var i = 0; i < text.length; i++) {
+			if (letter[i] !== undefined) {
+				letter[i].destroy();
+			}
 		}
 	};
 
