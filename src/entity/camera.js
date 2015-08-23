cfa4829b8c6afa81dba77c420aea11844aab363b
Jordan Lord <jordan@macmini>
2015-08-23 20:26:35 +0100

    + Camera.js

---
+++ src/entity/camera.js
@@ -0,0 +1,21 @@
+var Camera = function() {
+	var mod = Entity();
+
+
+	mod.following;
+
+	mod.update = function() {
+		mod.sprite.direction = (Math.PI / 2) + Math.atan2(mod.following.sprite.y - mod.sprite.y, mod.following.sprite.x - mod.sprite.x);
+		mod.sprite.speed = Math.sqrt(Math.pow(mod.following.sprite.y - mod.sprite.y, 2) + Math.pow(mod.following.sprite.x - mod.sprite.x, 2)) / 1000;
+	};
+
+	mod.destroy = function() {
+		mod.sprite.x = 0;
+		mod.sprite.y = 0;
+
+		mod.update = function() {};
+	};
+
+
+	return mod;
+};
