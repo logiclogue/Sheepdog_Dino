e96b10f1d9826288a5512bd8490566d459bfe376
Jordan Lord <jordanpaullord@hotmail.co.uk>
2015-08-24 11:16:28 +0100

    = Updated about page

--- src/scene/about.js
+++ src/scene/about.js
@@ -1,6 +1,10 @@
 var About = function() {
 	game.Rect(-xOffset, -yOffset, gameWidth, gameHeight, "#000000");
 
+	Text("created in 72 hours", 64, 64);
+	Text("for ludum dare 33", 64, 72);
+	Text("copyright jordan lord 2015", 64, 80);
+
 	var mod = Menu(140, 100, ["back"], [
 		function() {
 			global.sprites = [];
